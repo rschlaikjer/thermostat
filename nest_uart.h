8ab27bc3a6fc5f5cb62cdfb7b809ff240330adec
Ross Schlaikjer <ross@schlaikjer.net>
2018-09-01 16:57:43 -0400

    Make uart callable from C

--- nest_uart.h
+++ nest_uart.h
@@ -9,14 +9,24 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
 
+#ifdef __cplusplus
 const uint32_t NEST_UART = USART2;
-
+extern "C" {
 void uart_setup(void);
 void uart_puts(const char *string);
 void uart_putln(const char *string);
 void uart_putd(size_t i);
-void uart_putd(ssize_t i);
 void uart_putf(double n, uint8_t precision);
 void uart_putx(size_t i);
+}
+void uart_putd(ssize_t i);
+#else
+void uart_setup(void);
+void uart_puts(const char *string);
+void uart_putln(const char *string);
+void uart_putd(size_t i);
+void uart_putf(double n, uint8_t precision);
+void uart_putx(size_t i);
+#endif
 
 #endif // NEST_UART_H
